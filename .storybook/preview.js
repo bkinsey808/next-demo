e03468491a8d385709f0e809befcf0805f3902c0
Ben Kinsey <bkinsey@gmail.com>
2022-02-26 09:07:26 -1000

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import * as NextImage from "next/image";
+
+import "../src/styles/globals.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
---
+++ src/components/HomeContent.stories.tsx
@@ -0,0 +1,8 @@
+import HomeContent from "./HomeContent";
+
+export default {
+  title: "Pages/Home",
+  component: HomeContent,
+};
+
+export const HomePage = () => <HomeContent />;
