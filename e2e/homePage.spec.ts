d92cbafd845baee835ca3d28b5a5470c0de8bbfc
Ben Kinsey <bkinsey@gmail.com>
2022-03-04 20:12:45 -1000

    feat: add visual regression testing for e2e pages

--- e2e/homePage.spec.ts
+++ e2e/homePage.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("basic test", async ({ page }) => {
+test("Home Page test", async ({ page }) => {
   await page.goto("localhost:3000");
   await expect(page).toHaveTitle(/Create Next App/);
+  expect(await page.screenshot()).toMatchSnapshot("home_page.png");
 });
--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -15,7 +15,7 @@ const HomeContent: FC = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!!</a>
         </h1>
 
         <p className={styles.description}>
