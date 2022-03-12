45a875b55bfb1946a321bac369251589bb84c777
Ben Kinsey <bkinsey@gmail.com>
2022-03-12 07:59:24 -1000

    feat: add tailwind

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "string-quotes": "double",
     "declaration-colon-newline-after": null, // not compatible with prettier
     "value-list-comma-newline-after": null, // not compatible with prettier
-    "custom-property-pattern": "^[a-z]+[A-Z0-9][a-z0-9]+[A-Za-z0-9]*$", // camelCase
+    "custom-property-pattern": "^[a-z]+[A-Z0-9][a-z0-9]+[A-Za-z0-9]*$", // camelCase,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
   },
 };
