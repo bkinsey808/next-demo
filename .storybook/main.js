84c531f6472cc3abf2a6424e62f6ed9908a8d11d
Ben Kinsey <bkinsey@gmail.com>
2022-03-12 23:42:52 -1000

    feat(storybook): add storybook accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     {
       /**
        * Fix Storybook issue with PostCSS@8
