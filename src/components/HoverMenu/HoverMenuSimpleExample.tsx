edd1167440f84f900786ce4cc2b0d584bc748640
Ben Kinsey <bkinsey@gmail.com>
2022-03-21 05:25:36 -1000

    fix: make HoverMenu example more consistent

--- src/components/HoverMenu/HoverMenuSimpleExample.tsx
+++ src/components/HoverMenu/HoverMenuSimpleExample.tsx
@@ -7,44 +7,46 @@ export interface ConfigData {
   label: string;
 }
 
-export const enum OptionEnum {
+export const enum ColorEnum {
   GREEN = "green",
   YELLOW = "yellow",
   RED = "red",
 }
 
-export const ITEMS: OptionEnum[] = [
-  OptionEnum.GREEN,
-  OptionEnum.YELLOW,
-  OptionEnum.RED,
+export const ITEMS: ColorEnum[] = [
+  ColorEnum.GREEN,
+  ColorEnum.YELLOW,
+  ColorEnum.RED,
 ];
 
-export const ITEM_CONFIG: MenuItemConfig<ConfigData, OptionEnum> = {
-  [OptionEnum.GREEN]: {
+export const ITEM_CONFIG: MenuItemConfig<ConfigData, ColorEnum> = {
+  [ColorEnum.GREEN]: {
     label: "Green",
   },
-  [OptionEnum.RED]: {
+  [ColorEnum.RED]: {
     label: "Red",
   },
-  [OptionEnum.YELLOW]: {
+  [ColorEnum.YELLOW]: {
     label: "Yellow",
   },
 } as const;
 
 export const HoverMenuSimpleExample: FC = () => {
-  const [activeColor, setActiveColor] = useState<OptionEnum>(OptionEnum.GREEN);
+  const [activeColor, setActiveColor] = useState<ColorEnum>(ColorEnum.GREEN);
 
   return (
-    <HoverMenu<ConfigData, OptionEnum>
+    <HoverMenu<ConfigData, ColorEnum>
       activeKey={activeColor}
       items={ITEMS}
       itemConfig={ITEM_CONFIG}
       onSelect={(key) => setActiveColor(key)}
       ariaLabel={"Select Color"}
-      MenuButtonComponent={({ activeKey }) => <div>{activeKey}</div>}
+      MenuButtonComponent={({ activeKey }) => (
+        <div>{ITEM_CONFIG[activeKey].label}</div>
+      )}
       MenuItemComponent={({ itemKey, itemConfig, active, selected }) => (
         <div>
-          {itemConfig[itemKey]}: {JSON.stringify(active && selected)}
+          {itemConfig[itemKey].label}: {JSON.stringify(active && selected)}
         </div>
       )}
     />
