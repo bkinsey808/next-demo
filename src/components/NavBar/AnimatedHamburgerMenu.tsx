5aeb42f99484e952f3d680eb11d06d4331d1faa5
Ben Kinsey <bkinsey@gmail.com>
2022-03-28 00:30:45 -1000

    refactor(header): improve hamburger menu

--- src/components/NavBar/AnimatedHamburgerMenu.tsx
+++ src/components/NavBar/AnimatedHamburgerMenu.tsx
@@ -2,13 +2,11 @@ import clsx from "clsx";
 import cntl from "cntl";
 import { Dispatch, FC, SetStateAction } from "react";
 
-const genericHamburgerLineCN = (isOpen: boolean) =>
-  clsx(
-    cntl`h-1 w-6 my-1 rounded-full bg-black transition ease-in transform duration-300`,
-    {
-      [cntl`opacity-50 group-hover:opacity-100`]: !isOpen,
-    }
-  );
+const hamburgerMenuLineIsOpenCNs = [
+  cntl`translate-y-3 rotate-45 opacity-50 group-hover:opacity-100`,
+  cntl`opacity-0`,
+  cntl`-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100`,
+];
 
 export const AnimatedHamburgerMenu: FC<{
   isOpen: boolean;
@@ -19,22 +17,18 @@ export const AnimatedHamburgerMenu: FC<{
       className="group flex h-12 w-12 flex-col items-center justify-center rounded border-2 border-black"
       onClick={() => setIsOpen(!isOpen)}
     >
-      <div
-        className={clsx(`${genericHamburgerLineCN(isOpen)}`, {
-          "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100": isOpen,
-        })}
-      />
-      <div
-        className={clsx(`${genericHamburgerLineCN(isOpen)}`, {
-          "opacity-0": isOpen,
-        })}
-      />
-      <div
-        className={clsx(`${genericHamburgerLineCN(isOpen)}`, {
-          "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100":
-            isOpen,
-        })}
-      />
+      {hamburgerMenuLineIsOpenCNs.map((isOpenCN, index) => (
+        <div
+          key={index}
+          className={clsx(
+            "my-1 h-1 w-6 transform rounded-full bg-black transition duration-300 ease-in",
+            {
+              [isOpenCN]: isOpen,
+              "opacity-50 group-hover:opacity-100": !isOpen,
+            }
+          )}
+        />
+      ))}
     </button>
   );
 };
